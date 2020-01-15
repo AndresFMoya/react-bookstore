e39507a6d101d81c71d6caa6b0fdcd33ddbb3673
AndresFMoya <andresfmoya@gmail.com>
2020-01-15 16:18:16 -0500

    fix: move BooksForm to containers folder

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
-import BooksForm from './BooksForm';
+import BooksForm from '../containers/BooksForm';
 
 const App = () => (
   <div className="app">
