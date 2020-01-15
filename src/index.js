5610fd947c36af4047bfc41742bc58e27d0ae19a
AndresFMoya <andresfmoya@gmail.com>
2020-01-15 14:01:07 -0500

    feat: Initialize the Redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/App";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
 import './index.css';
+import rootReducer from './reducers';
+
+const randomId = () => Math.floor(Math.random() * 10 ** 6);
+
+const INITIAL_STATE = {
+  books: [
+    { id: randomId(), title: 'Book # 1 Title', category: 'biography' },
+    { id: randomId(), title: 'Book # 2 Title', category: 'kids' },
+    { id: randomId(), title: 'Book # 3 Title', category: 'learning' },
+  ],
+};
+
+const store = createStore(rootReducer, INITIAL_STATE);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
