53bf2a7c439bda4998771c38c44fba7207f6eb40
AndresFMoya <andresfmoya@gmail.com>
2020-01-17 08:06:49 -0500

    chore: add bootstrap CDN

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import './index.css';
+import './styles/scss/index.scss';
 import rootReducer from './reducers';
 import randomId from './components/randomId';
 
